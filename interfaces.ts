1e6a77ebb30049a88a10471a10fb5ce5988b2a54
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-11 22:44:43 +0200

    refactor - added method

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,23 @@
 interface Vehicle {
-    name: string,
-    year: number,
-    broken: boolean
+  name: string;
+  year: number;
+  broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
-    name: 'civic',
-    year: 2000,
-    broken: true
+  name: 'civic',
+  year: 2000,
+  broken: false,
+  summary(): string {
+    return `This is a ${this.name} made in ${this.year} and it is ${
+      this.broken ? 'broken' : 'running like a charm'
+    }.`;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-    console.log(`Name: ${vehicle.name}`);
-    console.log(`Year: ${vehicle.year}`);
-    console.log(`Is broken: ${vehicle.broken?'yes':'no'}`);
+  console.log(`${vehicle.summary()}`);
 };
 
 printVehicle(oldCivic);
