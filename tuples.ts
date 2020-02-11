f54e9ee6ecacc12171a60ee4ef6d53bedb99529f
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-02-11 15:57:52 +0200

    added some small exercises and comments for tuples

--- tuples.ts
+++ tuples.ts
@@ -8,4 +8,16 @@ const drink = {
 // Tuples are useful for csv file processing
 type Drink = [string, boolean, number];
 const pepsi: Drink = ['browm', true, 40];
+const tea: Drink = ['red', false, 10];
+const espresso: Drink = ['black', false, 0];
 console.log(pepsi);
+console.log(espresso);
+console.log(tea);
+
+// Tuple vs object
+// Standard - objects are preferred
+const carSpecs: [number, number] = [400, 3354];
+const carStats = {
+    horsePower: 400,
+    weight: 3354
+};
