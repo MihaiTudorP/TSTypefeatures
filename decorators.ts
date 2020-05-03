56b8c7b92eee084179dd7a736e6470c935e738d9
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2020-05-03 19:27:50 +0300

    Added decorators

---
+++ decorators.ts
@@ -0,0 +1,27 @@
+class Boat {
+  color: string = 'red';
+
+  get formattedColor(): string {
+    return `This boat's color is ${this.color}`;
+  }
+
+  @logError('Oops, boat was sunk in ocean!')
+  pilot(): void {
+    throw new Error();
+  }
+}
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
+
+new Boat().pilot();
